7c9998bb3bd09302985f8b8299676510f72f6b4e
suzukey <suzukei0208@gmail.com>
2021-02-08 19:52:42 +0900

    🎨 Rewrite to script setup

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,29 +1,32 @@
 <template>
   <h1>{{ msg }}</h1>
 
+  <button @click="count++">count is: {{ count }}</button>
   <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Docs</a> |
+    Edit
+    <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+
+  <p>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">
+      Vite Docs
+    </a>
+    |
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
   </p>
 </template>
 
-<script lang="ts">
-import { ref, defineComponent } from 'vue'
-export default defineComponent({
-  name: 'HelloWorld',
-  props: {
-    msg: {
-      type: String,
-      required: true
-    }
+<script setup lang="ts">
+import { ref, defineProps } from 'vue'
+
+defineProps({
+  msg: {
+    type: String,
+    required: true,
   },
-  setup: () => {
-    const count = ref(0)
-    const useScriptSetup = ref(false);
-    const useTsPlugin = ref(false);
-    return { count, useScriptSetup, useTsPlugin }
-  }
 })
+
+const count = ref(0)
 </script>
 
 <style scoped>
